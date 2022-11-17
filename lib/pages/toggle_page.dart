50163d20602c507538780be11da365ba0767adb9
hideokawahara <kawahide.72@gmail.com>
2022-11-17 23:46:08 +0900

    feat: トグル強度追加

--- lib/pages/toggle_page.dart
+++ lib/pages/toggle_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
 
 class TogglePage extends StatelessWidget {
   const TogglePage({Key? key}) : super(key: key);
@@ -24,18 +25,90 @@ class TogglePageBody extends StatefulWidget {
 
 class _TogglePageBodyState extends State<TogglePageBody> {
   bool isOn = false;
+  TapFeedBack isSelect = TapFeedBack.weak;
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: CupertinoSwitch(
-        value: isOn,
-        onChanged: (bool value) {
+    List<Widget> buttonList = TapFeedBack.values.map((select) {
+      final String buttonTitle;
+      switch (select) {
+        case TapFeedBack.weak:
+          buttonTitle = '弱';
+          break;
+        case TapFeedBack.medium:
+          buttonTitle = '中';
+          break;
+        case TapFeedBack.strong:
+          buttonTitle = '強';
+          break;
+        case TapFeedBack.vibe:
+          buttonTitle = '震';
+          break;
+      }
+      return FloatingActionButton(
+        heroTag: buttonTitle,
+        backgroundColor: isSelect == select ? Colors.grey : null,
+        child: Text(buttonTitle),
+        onPressed: () {
           setState(() {
-            isOn = value;
+            isSelect = select;
           });
         },
-      ),
+      );
+    }).toList();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+          child: SizedBox(
+            height: 200,
+            width: 200,
+            child: FittedBox(
+              fit: BoxFit.contain,
+              child: CupertinoSwitch(
+                value: isOn,
+                onChanged: (bool value) async {
+                  await tapFeedBackAction(isSelect);
+                  setState(() {
+                    isOn = value;
+                  });
+                },
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 56,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: buttonList,
+        ),
+      ],
     );
   }
+
+  Future<void> tapFeedBackAction(TapFeedBack selectFeedback) async {
+    switch (selectFeedback) {
+      case TapFeedBack.weak:
+        await HapticFeedback.selectionClick();
+        break;
+      case TapFeedBack.medium:
+        await HapticFeedback.lightImpact();
+        break;
+      case TapFeedBack.strong:
+        await HapticFeedback.heavyImpact();
+        break;
+      case TapFeedBack.vibe:
+        await HapticFeedback.vibrate();
+        break;
+    }
+  }
+}
+
+enum TapFeedBack {
+  weak,
+  medium,
+  strong,
+  vibe,
 }
