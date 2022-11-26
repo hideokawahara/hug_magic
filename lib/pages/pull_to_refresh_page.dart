3ac180670e412fe857a653d2c270385f4a8f1e5a
hideokawahara <kawahide.72@gmail.com>
2022-11-26 15:28:06 +0900

    feat: 秒数選択ピッカー作成

--- lib/pages/pull_to_refresh_page.dart
+++ lib/pages/pull_to_refresh_page.dart
@@ -1,7 +1,16 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class PullToRefreshPage extends StatelessWidget {
+class PullToRefreshPage extends StatefulWidget {
   const PullToRefreshPage({Key? key}) : super(key: key);
+
+  @override
+  State<PullToRefreshPage> createState() => _PullToRefreshPageState();
+}
+
+class _PullToRefreshPageState extends State<PullToRefreshPage> {
+  int seconds = 2;
+  final List<int> _items = [1, 2, 3, 4, 5];
 
   @override
   Widget build(BuildContext context) {
@@ -9,8 +18,65 @@ class PullToRefreshPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Puller'),
       ),
+      endDrawer: Drawer(
+        child: settingList(context),
+      ),
       body: const PullToRefreshPageBody(),
     );
+  }
+
+  Widget settingList(BuildContext context) {
+    return SafeArea(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              const Text('秒数'),
+              InkWell(
+                onTap: () {
+                  _showModalPicker(context);
+                },
+                child: Text('$seconds秒'),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  void _showModalPicker(BuildContext rootContext) {
+    showModalBottomSheet(
+      context: rootContext,
+      builder: (BuildContext context) {
+        return Container(
+          height: MediaQuery.of(context).size.height / 3,
+          child: GestureDetector(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: CupertinoPicker(
+              itemExtent: 40,
+              onSelectedItemChanged: _onSelectedItemChanged,
+              children: _items.map((item) => _pickerItem(item)).toList(),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _pickerItem(int second) {
+    return Text('$second秒');
+  }
+
+  void _onSelectedItemChanged(int index) {
+    setState(() {
+      seconds = _items[index];
+    });
   }
 }
 
